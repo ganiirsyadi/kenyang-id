5d4efb4fd0fc6fa87453c07b6f2bf57332494439
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2021-11-24 11:45:16 +0700

    setup supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
+import { supabase } from "../utils/supabaseClient"
 
 const Home: NextPage = () => {
+
+  useEffect(() => {
+    const testGetData = async () => {
+      const res = await supabase.from("test").select("*").single()
+      console.log(res)
+    }
+    testGetData()
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
